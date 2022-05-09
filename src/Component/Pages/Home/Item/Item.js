f0c3b1df728742d2b4ce1d77bf4ba4a4ade62619
fahad09fahim <anik.fahim07@gmail.com>
2022-05-09 09:51:32 +0600

    navbar and items updated

--- src/Component/Pages/Home/Item/Item.js
+++ src/Component/Pages/Home/Item/Item.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Item.css";
 const Item = ({ item }) => {
-  const { name, price, description, img } = item;
+  const { name, price, description, img, quantity, supplier } = item;
   return (
     <div className="item">
       <img src={img} alt="" />
@@ -10,6 +10,9 @@ const Item = ({ item }) => {
       <p className="description">
         <small>{description}</small>
       </p>
+      <p>Supplier Name: {supplier}</p>
+      <p>Quantity: {quantity}</p>
+      <button className="btn btn-info">Update</button>
     </div>
   );
 };
--- src/Component/Pages/Shared/Header/Header.js
+++ src/Component/Pages/Shared/Header/Header.js
@@ -5,25 +5,34 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div>
-      <Navbar bg="dark" variant="dark">
+      <Navbar
+        bg="dark"
+        sticky="top"
+        collapseOnSelect
+        expand="lg"
+        variant="dark"
+      >
         <Container>
           <Navbar.Brand as={Link} to="/">
             GO PORT
           </Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">
-              Home
-            </Nav.Link>
-            <Nav.Link as={Link} to="/items">
-              Items
-            </Nav.Link>
-            <Nav.Link href="#pricing">Survey</Nav.Link>
-            <Nav.Link href="#pricing">Trusted Shop</Nav.Link>
-          </Nav>
-          <Nav className="me">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="/items">
+                Items
+              </Nav.Link>
+              <Nav.Link href="#pricing">Survey</Nav.Link>
+              <Nav.Link href="#pricing">Trusted Shop</Nav.Link>
+            </Nav>
+            <Nav className="me">
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </div>
